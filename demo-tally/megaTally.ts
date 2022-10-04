4fe34032a25b5a4d4f34fb9d3b9b96da988a6c92
Terence Siganakis <terence@growingdata.com.au>
2022-10-04 21:07:53 +1100

    Minor fixes

--- demo-tally/megaTally.ts
+++ demo-tally/megaTally.ts
@@ -2,7 +2,7 @@ import { DispatchAgent } from "@blockfold/dispatch";
 
 let agent = new DispatchAgent();
 
-agent.onEvent("tally", (proj, agent, event, state): Promise<any> => {
+agent.onEvent("tally", (context, event, state): Promise<any> => {
 	// Initialize our state (if its not ready)
 	if (!state.tally) {
 		state.tally = 0;
@@ -11,19 +11,19 @@ agent.onEvent("tally", (proj, agent, event, state): Promise<any> => {
 	if (event.eventName == "Transfer") {
 		var value = agent.fromERC20BigNum(event.data["value"]);
 		state.tally += value;
-		console.log("tally.onEvent.Transfer", value, state.tally);
+		context.agent.logMessage("INFO", `tally.onEvent.Transfer: ${value}, ${state.tally}`);
 	}
 
 	return Promise.resolve(state);
 });
 
-agent.onCron("tally", "* * * * *", (proj, agent, event, state): Promise<any> => {
+agent.onCron("tally", "* * * * *", (context, event, state): Promise<any> => {
 	// Initialize our state (if its not ready)
 	if (!state.tally) {
 		state.tally = 0;
 	}
 	// Reset the state:
-	console.log("tally.onCron.(each minute)", state.tally);
+	context.agent.logMessage("INFO", `tally.onCron: ${state.tally}`);
 	state.tally = 0;
 
 	return Promise.resolve(state);
