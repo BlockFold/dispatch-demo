e78c8975ef15f7b035d2613807c1665618863802
Terence Siganakis <terence@growingdata.com.au>
2022-10-06 06:21:25 +1100

    Fromatting, no code to constructor

--- demo-tally/megaTally.ts
+++ demo-tally/megaTally.ts
@@ -1,32 +1,32 @@
 import { Bot } from "@blockfold/dispatch";
 
-let bot = new Bot("tally");
+let bot = new Bot();
 
 bot.onEvent((event, state): Promise<any> => {
-	// Initialize our state (if its not ready)
-	if (!state.tally) {
-		state.tally = 0;
-	}
+  // Initialize our state (if its not ready)
+  if (!state.tally) {
+    state.tally = 0;
+  }
 
-	if (event.eventName == "Transfer") {
-		var value = bot.fromBigNum(event.data["value"], 6);
-		state.tally += value;
-		bot.logMessage("INFO", `tally.onEvent.Transfer: ${value}, ${state.tally}`);
-	}
+  if (event.eventName == "Transfer") {
+    var value = bot.fromBigNum(event.data["value"], 6);
+    state.tally += value;
+    bot.logMessage("INFO", `tally.onEvent.Transfer: ${value}, ${state.tally}`);
+  }
 
-	return Promise.resolve(state);
+  return Promise.resolve(state);
 });
 
 bot.onCron("* * * * *", (event, state): Promise<any> => {
-	// Initialize our state (if its not ready)
-	if (!state.tally) {
-		state.tally = 0;
-	}
-	// Reset the state:
-	bot.logMessage("INFO", `tally.onCron: ${state.tally}`);
-	state.tally = 0;
+  // Initialize our state (if its not ready)
+  if (!state.tally) {
+    state.tally = 0;
+  }
+  // Reset the state:
+  bot.logMessage("INFO", `tally.onCron: ${state.tally}`);
+  state.tally = 0;
 
-	return Promise.resolve(state);
+  return Promise.resolve(state);
 });
 
 export default bot;
